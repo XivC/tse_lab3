68e522a467ff62756cc8ba05bdefa3e7594e34e7
Artem Dybov <artem.dybov@webium.team>
2023-04-25 22:53:10 +0300

    rhtwthrwwthr

---
+++ src/test/java/ru/ifmo/firefox/TestAuth.java
@@ -0,0 +1,41 @@
+package ru.ifmo.firefox;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TestAuth {
+
+
+  WebDriver driver;
+  @FindBy(xpath = "/html/body/div[2]/div[1]/div[2]/main/div/div/main/div[1]/div/div[1]/button")
+  WebElement manButton;
+
+  @BeforeEach
+  public void setUp() {
+    driver = Runner.forChrome();
+    int a = 1;
+  }
+
+  @AfterEach
+  public void exit() {
+    driver.close();
+    driver.quit();
+  }
+
+  @Test
+  public void testButtonsText() {
+    driver.get("https://mamba.ru/");
+    PageFactory.initElements(driver, this);
+    String text = manButton.getText();
+    assert Objects.equals(text, "Мужчина");
+  }
+
+}
